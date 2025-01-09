e7766e0b6cf6318a159ef92918096625bfb6c639
firatgoktepe <firatgoktepe1986@gmail.com>
2025-01-09 19:47:44 +0300

    fix: mobile button touch more

--- app/components/promptsuggestionbtn.tsx
+++ app/components/promptsuggestionbtn.tsx
@@ -1,10 +1,9 @@
-const PromptSuggestionBtn = ({text,onClick})=>{
-    return (
-        <button className="prompt-sug-btn" onClick={onClick}>
-            {text}
-        </button>
-    )
-}
+const PromptSuggestionBtn = ({ text, onClick }) => {
+  return (
+    <button type="button" className="prompt-sug-btn" onClick={onClick}>
+      {text}
+    </button>
+  );
+};
 
-
-export default PromptSuggestionBtn
+export default PromptSuggestionBtn;
